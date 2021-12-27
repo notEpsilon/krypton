9bd166102b88327e57da2e4c92c41fbe6ee80ce3
notEpsilon <ibrahimnasr393@gmail.com>
2021-12-27 18:53:34 +0200

    initialized some server main directories & configured firebase project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Krypton
+Krypton is a modern solution to university registration systems with cloud-based NoSQL database that scales automatically.
---
+++ server/src/firebase/firebase.util.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+export {
+    firestore,
+    auth
+};
