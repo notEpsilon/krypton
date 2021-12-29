088f9a3804449fd61eb5025e9ba44b7151a83641
notEpsilon <ibrahimnasr393@gmail.com>
2021-12-29 08:10:03 +0200

    added some request handlers (without considering their folder structure)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,41 @@
-console.log("hello, world!");
+import express, { Request, Response } from 'express';
+import { firestore } from './firebase/firebase.util';
+import { collection, doc, addDoc, getDocs, getDoc, DocumentData } from 'firebase/firestore';
+import Student from './models/Student.model';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+// get all students
+app.get('/students', async (req: Request, res: Response) => {
+    const colRef = collection(firestore, 'students');
+    
+    try {
+        const StudentDocs = await getDocs(colRef);
+        let students: DocumentData = [];
+        StudentDocs.forEach(student => students.push(student.data()));
+        res.status(200).send(students);
+    }
+    catch (err) {
+        res.status(502).send("Error Retrieving Students Documents.");
+    }
+});
+
+// add a student
+app.post('/students', async (req: Request, res: Response) => {
+    const colRef = collection(firestore, 'students');
+    
+    const studentInfo = new Student({ ...req.body }).info();
+    
+    try {
+        await addDoc(colRef, studentInfo);
+        res.status(200).send("Student Created Successfully.");
+    }
+    catch (err) {
+        res.status(502).send("Error Adding a Student Document.");
+    }
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}...`));
--- server/src/models/Student.model.ts
+++ server/src/models/Student.model.ts
@@ -1,1 +1,28 @@
-export class Student {}
+export type StudentInfo = {
+    userID: string
+    password: string
+    isVerified: boolean
+    type?: 0
+};
+
+export default class Student {
+    public userID: string = "";
+    public password: string = "";
+    public isVerified: boolean = false;
+    public static readonly type: 0 = 0;
+
+    constructor(student: StudentInfo) {
+        this.userID = student.userID;
+        this.password = student.password;
+        this.isVerified = student.isVerified;
+    }
+
+    public info(): StudentInfo {
+        return {
+            userID: this.userID,
+            password: this.password,
+            isVerified: this.isVerified,
+            type: Student.type
+        };
+    }
+}
